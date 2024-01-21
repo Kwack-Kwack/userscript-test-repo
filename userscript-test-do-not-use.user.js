0a2a198814dc3fef8ab3446b1b1939c7c58d384b
Kwack <kwack@kwack.dev>
2024-01-21 18:17:31 +0800

    Bump version

--- userscript-test-do-not-use.user.js
+++ userscript-test-do-not-use.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         userscript-test-do-not-use
 // @namespace    dev.kwack.scripts
-// @version      0.0.1
+// @version      0.0.2
 // @description  try to take over the world!
 // @author       Kwack [2190604]
 // @match        http://*/*
