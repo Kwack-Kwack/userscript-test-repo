769450059c9ad641729b6fae1ab861c93fe65560
Kwack <kwack@kwack.dev>
2024-01-21 20:08:33 +0800

    Update userscript-test-do-not-use.user.js

--- userscript-test-do-not-use.user.js
+++ userscript-test-do-not-use.user.js
@@ -4,7 +4,7 @@
 // @version      0.0.2
 // @description  try to take over the world!
 // @author       Kwack [2190604]
-// @match        http://*/*
+// @match        https://www.torn.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
