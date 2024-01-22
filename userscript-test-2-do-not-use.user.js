74abb35b7f81dcf4fecc73e515b803af0ac6f4cf
Kwack <kwack@kwack.dev>
2024-01-23 03:00:51 +1100

    Add second script

---
+++ userscript-test-2-do-not-use.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         userscript-test-do-not-use
+// @namespace    dev.kwack.scripts
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       Kwack [2190604]
+// @match        https://www.torn.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("Script!!");
+})();
