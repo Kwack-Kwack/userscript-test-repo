f71ed763b294926be80154dcca29a2614adaf91b
Kwack <kwack@kwack.dev>
2024-01-23 01:14:31 +0800

    Bump v1 version

--- userscript-test-do-not-use.user.js
+++ userscript-test-do-not-use.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         userscript-test-do-not-use
 // @namespace    dev.kwack.scripts
-// @version      0.0.4
+// @version      0.0.5
 // @description  try to take over the world!
 // @author       Kwack [2190604]
 // @match        https://www.torn.com/*
@@ -12,5 +12,5 @@
 (function() {
     'use strict';
 
-    console.log("Script!!");
+    console.log("Script with changes!!");
 })();
